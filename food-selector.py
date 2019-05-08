b7337ce7f0e4f930204b1fe0e84f5084ede0fd1e
parjanya <pbrahmac@gmail.com>
2019-05-08 11:15:48 -0700

    33507

--- food-selector.py
+++ food-selector.py
@@ -1,4 +1,6 @@
+import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 33507))
+    app.run(debug=False, port=port, host='0.0.0.0')
