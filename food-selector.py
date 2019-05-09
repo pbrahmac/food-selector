cd072945aaff76213d0f0c9578ce1815dce936a8
parjanya <pbrahmac@gmail.com>
2019-05-08 21:42:17 -0700

    Oh bruh

--- food-selector.py
+++ food-selector.py
@@ -3,4 +3,4 @@ from app import app
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 33507))
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=True, port=port, host='0.0.0.0')
